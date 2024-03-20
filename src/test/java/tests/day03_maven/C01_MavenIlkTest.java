0039b892f95a1c39f2613417059f24058bf665ad
murat akar <akar.murat@hotmail.com>
2024-03-20 14:48:40 +0300

    3. gun sonu

---
+++ src/test/java/tests/day03_maven/C01_MavenIlkTest.java
@@ -0,0 +1,23 @@
+package tests.day03_maven;
+
+import org.checkerframework.checker.units.qual.C;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import utilities.ReusableMethods;
+
+import java.time.Duration;
+
+public class C01_MavenIlkTest {
+    public static void main(String[] args) {
+
+        WebDriver driver = new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+
+        driver.get("https://www.testotomasyonu.com/");
+
+        ReusableMethods.bekle(3);
+
+        driver.quit();
+    }
+}
---
+++ src/test/java/tests/day03_maven/C02_ExternalWebDriverKullanma.java
@@ -0,0 +1,10 @@
+package tests.day03_maven;
+
+public class C02_ExternalWebDriverKullanma {
+
+    /*
+
+     */
+
+
+}
